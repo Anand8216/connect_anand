e81a164b4703aec04531602f171d95cabee7b597
Anand <anandshubham8216@gmail.com>
2023-08-07 13:38:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
+import About from './components/About';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio/Portfolio';
+import Services from './components/Services/Services';
+import Social from './social/Social';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Home/>
+      <Social/>
+      <Navbar/>
+      <About/>
+      <Portfolio/>
+      <Services/>
+      <Contact/>
+      <Footer/>
+      
+     
+     
+    </>
   );
 }
 
